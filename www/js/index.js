d5af93bbe92176d19c029988a22c4028100b7f98
Andres Berardoni <andres.berardoni@engee.com.ar>
2013-11-19 15:50:30 -0300

    test ajax post crossdomind

--- www/js/index.js
+++ www/js/index.js
@@ -1,21 +1,30 @@
-$( document ).bind( "mobileinit", function() {
-    // Make your jQuery Mobile framework configuration changes here!
-
-    $.mobile.allowCrossDomainPages = true;
-	$.support.cors = true;
-});
- 
- function capturePhoto()
- {
+function capturePhoto() {
 	navigator.device.capture.captureImage();
- }
+}
  
 function init() {
-  document.addEventListener("deviceready", onDeviceReady, false);
-
+	$.mobile.allowCrossDomainPages = true;
+	document.addEventListener("deviceready", onDeviceReady, false);
 }
  
 function onDeviceReady() {
-  navigator.splashscreen.hide();
-
+	$.mobile.allowCrossDomainPages = true;
+	url = 'http://www.priorplace.com/services/event_actions.php';
+	$.ajax({
+         type: 'POST',
+         url: url,
+         contentType: "application/json",
+         dataType: 'json',
+         data: {action: 'get_older_posts', event_id: 53, start: 5 }, 
+         success: function(res) {  
+           alert("asdasd");
+           console.dir(res.status);
+         },  
+         error: function(e) {  
+           console.log(e.message);  
+         },  
+         complete: function(data) {  
+           console.log(e.message);  
+         }  
+       });  
 }
